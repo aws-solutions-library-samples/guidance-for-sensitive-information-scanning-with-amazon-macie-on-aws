246a4a1eb9c2ba03c42bdbc8e0613598afbdfe04
Prashob Krishnan <kprashob@amazon.com>
2025-08-08 09:22:47 -0600

    Adding Solution id to the CDK description

--- source/bin/solutions-guidance-macie.ts
+++ source/bin/solutions-guidance-macie.ts
@@ -5,7 +5,9 @@ import { AwsSolutionsChecks } from 'cdk-nag';
 import { SolutionsGuidanceMacieStack } from '../lib/solutions-guidance-macie-stack';
 
 const app = new cdk.App();
-new SolutionsGuidanceMacieStack(app, 'SolutionsGuidanceMacieStack');
+new SolutionsGuidanceMacieStack(app, 'SolutionsGuidanceMacieStack', {
+  description: 'Guidance for sensitive information scanning with Amazon Macie (SO9109)'
+});
 
 // Add CDK Nag AWS Solutions checks
 Aspects.of(app).add(new AwsSolutionsChecks());
